0e0f76dfd9d1662e11d86a39f7c0d7724989cf82
anna.deineko <anna.deineko@student.kdg.be>
2025-01-13 17:19:08 +0100

    fastapi post requeste corrected

--- statistics/app/main.py
+++ statistics/app/main.py
@@ -1,24 +1,10 @@
-from fastapi import FastAPI, HTTPException
-import logging
-from app.services.prediction_churn import predict_and_save_to_db
-
-logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+from fastapi import FastAPI
+from app.routes import router
 
 app = FastAPI()
 
-@app.post("/predict")
-async def trigger_prediction():
-    """
-    Endpoint to trigger the prediction process.
-    """
-    try:
-        logging.info("Prediction process started...")
-        predict_and_save_to_db()
-        logging.info("Prediction process completed successfully.")
-        return {"status": "success", "message": "Prediction process completed and data saved to the database."}
-    except FileNotFoundError as e:
-        logging.error(f"Error during prediction: {e}")
-        raise HTTPException(status_code=404, detail=str(e))
-    except Exception as e:
-        logging.error(f"Error during prediction: {e}")
-        raise HTTPException(status_code=500, detail="An error occurred during the prediction process.")
+app.include_router(router)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
